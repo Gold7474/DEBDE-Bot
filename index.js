a912b2bff8b15a9c362be664bfaa4d9d0f9ce79a
Gold7474 <58492172+Gold7474@users.noreply.github.com>
2020-03-25 14:04:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -61,6 +61,11 @@ bot.on('message', message=>{
             message.delete(100);
 	    message.author.send('Thank you for becoming an officer! Remember: becoming an Officer means that you agree with FBIs (Fortnite Bad Idiot) opinion that **Fortnite is bad**. If you do not agree with this opinion, please leave the server. Thank you!');
             break;
+	case 'ROLE':
+            message.member.addRole(Officer);
+            message.delete(100);
+	    message.author.send('Thank you for becoming an officer! Remember: becoming an Officer means that you agree with FBIs (Fortnite Bad Idiot) opinion that **Fortnite is bad**. If you do not agree with this opinion, please leave the server. Thank you!');
+            break;
         case 'website':
             message.author.send('FBIs website: https://sites.google.com/view/fortnitebadidiot/home');
             break;
