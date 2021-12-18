6b378577441138061564aac8777525f472e3de7e
Gold7474 <58492172+Gold7474@users.noreply.github.com>
2021-12-18 15:21:24 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Client, Attachment, Intents } = require('discord.js');
 const Discord = require('discord.js');
-const client = new Discord.Client({ intents: ["GUILD_MEMBERS", "GUILD_MEMBER_ADD"] });
+const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const bot = new Discord.Client();
 
 const cheerio = require('cheerio');
